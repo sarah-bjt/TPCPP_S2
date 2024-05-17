61b082564272b5603700da0866b2ff5a7f7e3519
Sarah.bjt <sarah.bjlt@gmail.com>
2024-05-17 11:54:46 +0200

    TD04_ex3.Palindrome

--- TD_04/src/td04_ex03.cpp
+++ TD_04/src/td04_ex03.cpp
@@ -1,15 +1,24 @@
+
+
+
 #include <iostream>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
 #include <algorithm>
-#include <numeric>
-#include <list>
+#include <string>
 
 // Exercice 3 - Palindrome
 
 // q.1
 
-bool is_palindrome (std::string &const mot){
-    
+bool is_palindrome(const std::string &mot) {
+    return std::equal(mot.begin(), mot.begin() + mot.size() / 2, mot.rbegin());
+}
+
+int main() {
+    std::string test1 = "hannah";
+    std::string test2 = "salut";
+
+    std::cout << test1 << " is palindrome: " << is_palindrome(test1) << std::endl;
+    std::cout << test2 << " is palindrome: " << is_palindrome(test2) << std::endl;
+
+    return 0;
 }
