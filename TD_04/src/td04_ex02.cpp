13227927f0120bee32104871732fc92177e03864
Sarah.bjt <sarah.bjlt@gmail.com>
2024-05-22 23:14:18 +0200

    TD04 exo2 et exo3

--- TD_04/src/td04_ex02.cpp
+++ TD_04/src/td04_ex02.cpp
@@ -19,23 +19,32 @@ int nbrLettre1erMot(std::string const& phrase){
 }
 
 // q.2
-std::vector<std::string> split_string(std::string const& str){
+std::vector<std::string> split_string(std::string const& str) {
     std::vector<std::string> tableau;
-    int lastStop{0};
-    for ( int i{0}; i<str.end(); i++){
-        if (is_space(i)){
+    auto start = str.begin();
+    auto last_stop = std::find(start, str.end(), ' ');
 
-        }
+    while (last_stop != str.end()) {
+        std::string new_word(start, last_stop);
+        tableau.push_back(new_word);
+        start = last_stop + 1;
+        last_stop = std::find(start, str.end(), ' ');
     }
-    
 
+    if (start != str.end()) {
+        tableau.push_back(std::string(start, str.end()));
+    }
+    return tableau;
 }
-
-
 
 int main(){
     std::string phrase {"coder une autre facon"};
     //q.1
-    std::cout<<"Le nombre de lettre dans le premier mot de "<< phrase << " est"<< nbrLettre1erMot(phrase)<<std::endl;
+    std::cout<<"Le nombre de lettre dans le premier mot de \""<< phrase << "\" est "<< nbrLettre1erMot(phrase)<<std::endl;
+    //q.2
+    std::vector<std::string> mots {split_string(phrase)};
+    for(auto i {mots.begin()}; i<mots.end(); i++){
+        std::cout << *i << std::endl;
+    }
 }
 
--- TD_04/src/td04_ex03.cpp
+++ TD_04/src/td04_ex03.cpp
@@ -1,6 +1,3 @@
-
-
-
 #include <iostream>
 #include <algorithm>
 #include <string>
